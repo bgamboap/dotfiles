efaca6b06bb2277caec519f9211c8a62f90c19e3
bgamboap <bgamboap@pm.me>
2021-02-10 16:09:11 +0000

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+
+#!/bin/zsh
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -r example-intel &
